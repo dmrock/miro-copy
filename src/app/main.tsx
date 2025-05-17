9910e53dbeb48eeb0e65058a27caeda7b13d6142
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-17 14:18:44 +0200

    feat: add mocks

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,8 +4,19 @@ import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import { router } from './router';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+async function enableMocking() {
+  if (import.meta.env.PROD) {
+    return;
+  }
+
+  const { worker } = await import('@/shared/api/mocks/browser');
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+});
--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,12 +1,16 @@
+import { rqClient } from '@/shared/api/instance';
+import { CONFIG } from '@/shared/model/config';
 import { ROUTES } from '@/shared/model/routs';
 import { href, Link } from 'react-router-dom';
 
 function BoardsListPage() {
+  const boardsQuery = rqClient.useQuery('get', '/boards');
+
   return (
     <div>
-      <h1>Boards list</h1>
+      <h1>Boards list {CONFIG.API_BASE_URL}</h1>
 
-      <Link to={href(ROUTES.BOARD, { boardId: '1' })}>Board 1</Link>
+      {boardsQuery.data?.map((board) => <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>)}
     </div>
   );
 }
