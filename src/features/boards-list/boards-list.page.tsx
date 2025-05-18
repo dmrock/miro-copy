545100cba1bb0dd0cfe71b1ef779f4bcbe883838
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-18 11:08:56 +0200

    feat: implement create and delete board functionality with mutations

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,16 +1,55 @@
 import { rqClient } from '@/shared/api/instance';
 import { CONFIG } from '@/shared/model/config';
 import { ROUTES } from '@/shared/model/routs';
+import { useQueryClient } from '@tanstack/react-query';
 import { href, Link } from 'react-router-dom';
 
 function BoardsListPage() {
+  const queryClient = useQueryClient();
   const boardsQuery = rqClient.useQuery('get', '/boards');
+
+  const createBoardMutation = rqClient.useMutation('post', '/boards', {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions('get', '/boards'));
+    },
+  });
+
+  const deleteBoardMutation = rqClient.useMutation('delete', '/boards/{boardId}', {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions('get', '/boards'));
+    },
+  });
 
   return (
     <div>
       <h1>Boards list {CONFIG.API_BASE_URL}</h1>
 
-      {boardsQuery.data?.map((board) => <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>)}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target as HTMLFormElement);
+          createBoardMutation.mutate({
+            body: { name: formData.get('name') as string },
+          });
+        }}>
+        <input name="name" />
+        <button type="submit" disabled={createBoardMutation.isPending}>
+          Create board
+        </button>
+      </form>
+
+      {boardsQuery.data?.map((board) => (
+        <div key={board.id}>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}</Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() => {
+              deleteBoardMutation.mutate({ params: { path: { boardId: board.id } } });
+            }}>
+            Delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
