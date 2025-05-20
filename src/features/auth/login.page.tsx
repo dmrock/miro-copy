460e9d537f279a907821b6cb5ecfe5291aa98ce2
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-20 22:07:54 +0200

    feat: add log in form

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,5 +1,24 @@
+import { ROUTES } from '@/shared/model/routs';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/kit/card';
+import { Link } from 'react-router-dom';
+
 function LoginPage() {
-  return <div>Login page</div>;
+  return (
+    <main className="grow flex flex-col pt-[200px] items-center">
+      <Card className="w-full max-w-[400px]">
+        <CardHeader>
+          <CardTitle>Log in</CardTitle>
+          <CardDescription>Enter your email and password</CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter>
+          <p>
+            Don't have an account? <Link to={ROUTES.REGISTER}>Register</Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
 }
 
 export const Component = LoginPage;
