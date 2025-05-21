e1455e7c997d918b270e9c0d44f1cd003f28f54d
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-21 19:38:36 +0200

    feat: implement AuthLayout component for login page

--- src/features/auth/login.page.tsx
+++ src/features/auth/login.page.tsx
@@ -1,23 +1,18 @@
 import { ROUTES } from '@/shared/model/routs';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/kit/card';
 import { Link } from 'react-router-dom';
+import { AuthLayout } from './auth-layout';
 
 function LoginPage() {
   return (
-    <main className="grow flex flex-col pt-[200px] items-center">
-      <Card className="w-full max-w-[400px]">
-        <CardHeader>
-          <CardTitle>Log in</CardTitle>
-          <CardDescription>Enter your email and password</CardDescription>
-        </CardHeader>
-        <CardContent></CardContent>
-        <CardFooter>
-          <p>
-            Don't have an account? <Link to={ROUTES.REGISTER}>Register</Link>
-          </p>
-        </CardFooter>
-      </Card>
-    </main>
+    <AuthLayout
+      title="Log in"
+      description="Enter your email and password"
+      form={<form></form>}
+      footerText={
+        <>
+          Don't have an account? <Link to={ROUTES.REGISTER}>Register</Link>
+        </>
+      }></AuthLayout>
   );
 }
 
