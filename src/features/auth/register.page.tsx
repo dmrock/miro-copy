c8de41f8e67799be139926a9eaed98c00371a037
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-05-22 22:25:30 +0200

    feat: implement RegisterForm component with validation and submission handling

--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,13 +1,14 @@
 import { ROUTES } from '@/shared/model/routs';
 import { Link } from 'react-router-dom';
 import { AuthLayout } from './auth-layout';
+import { RegisterForm } from './register-form';
 
 function RegisterPage() {
   return (
     <AuthLayout
       title="Register"
       description="Enter your email and password"
-      form={<form></form>}
+      form={<RegisterForm />}
       footerText={
         <>
           Already have an account? <Link to={ROUTES.LOGIN}>Log in</Link>
