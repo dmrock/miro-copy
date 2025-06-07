7a855b4f775fd3dcbb30c15a4820f233200d0fc8
Denis Sitnikov <denis.sitnikov@gostudent.org>
2025-06-07 15:11:06 +0200

    feat: update API client to publicRqClient and enhance session management with token return

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,10 +1,37 @@
 import { CONFIG } from '@/shared/model/config';
 import createFetchClient from 'openapi-fetch';
 import createClient from 'openapi-react-query';
-import type { ApiPaths } from './schema';
+import type { ApiPaths, ApiSchemas } from './schema';
+import { useSession } from '../model/session';
 
 export const fetchClient = createFetchClient<ApiPaths>({
   baseUrl: CONFIG.API_BASE_URL,
 });
-
 export const rqClient = createClient(fetchClient);
+
+export const publicFetchClient = createFetchClient<ApiPaths>({
+  baseUrl: CONFIG.API_BASE_URL,
+});
+export const publicRqClient = createClient(fetchClient);
+
+fetchClient.use({
+  onRequest({ request }) {
+    const token = useSession.getState().token;
+    if (token) {
+      request.headers.set('Authorization', `Bearer ${token}`);
+    } else {
+      return new Response(
+        JSON.stringify({
+          code: 'NOT_AUTHORIZED',
+          message: 'You are not authorized to access this response',
+        } as ApiSchemas['Error']),
+        {
+          status: 401,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+    }
+  },
+});
